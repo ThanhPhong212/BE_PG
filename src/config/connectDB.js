ea0fc914874f0b47117cb08285c1c449a25e65c5
ThanhPhong <bienthanhphong212@gmail.com>
2022-10-09 16:50:01 +0700

    update database

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -7,13 +7,11 @@ const sequelize = new Sequelize(process.env.DB_URI, {
   timezone: "+07:00",
 });
 let connectDB = async () => {
-
-
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    console.log("Connection has been established successfully");
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error("Unable to connect to the database");
   }
 };
 module.exports = connectDB;
