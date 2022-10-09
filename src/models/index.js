4cba5cb00761fc89f7a973f542c9d2359241165c
ThanhPhong <bienthanhphong212@gmail.com>
2022-10-09 16:51:13 +0700

    âfaf

--- src/models/index.js
+++ src/models/index.js
@@ -5,12 +5,12 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_URI, {
   logging: false,
   timezone: "+07:00",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000,
+  // },
 });
 
 const db = {};
